c1b166e2fd80f75c9332b935b9019340b44bd795
shambho <Sivakrishna.Karasala@eku.edu>
2019-03-22 21:05:45 -0400

    add recipelist component

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -3,11 +3,24 @@ import Recipe from "./Recipe";
 
 export default class RecipeList extends Component {
   render() {
+    const { recipes } = this.props;
     return (
-      <div>
-        Aum Namah Shivaya from Recipe List
-        <Recipe />
-      </div>
+      <>
+        <div className="container py-5">
+          {/* title */}
+          <div className="row">
+            <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
+              <h1 className="text-slanted">recipe list</h1>
+            </div>
+          </div>
+          {/* end of title */}
+          <div className="row">
+            {recipes.map(recipe => (
+              <Recipe key={recipe.recipe_id} recipe={recipe} />
+            ))}
+          </div>
+        </div>
+      </>
     );
   }
 }
