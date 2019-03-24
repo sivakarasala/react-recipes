78431215d33d78a72569344921e32369b10e328f
shambho <Sivakrishna.Karasala@eku.edu>
2019-03-23 20:14:20 -0400

    add image to recipe component

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,7 +1,26 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Recipe extends Component {
   render() {
-    return <div>Aum Namah Shivaya from Recipe</div>;
+    const {
+      image_url,
+      title,
+      source_url,
+      publisher,
+      recipe_id
+    } = this.props.recipe;
+    return (
+      <div className="col-10 mx-auto col-md-6 col-lg-4 my-3">
+        <div className="card" style={{ height: "100%" }}>
+          <img
+            src={image_url}
+            style={{ height: "14rem" }}
+            className="img-card-top"
+            alt="recipe"
+          />
+        </div>
+      </div>
+    );
   }
 }
