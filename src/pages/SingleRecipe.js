bc138a5a7252df18c6363b54f849b0de00ba4d3c
shambho <Sivakrishna.Karasala@eku.edu>
2019-03-23 21:12:28 -0400

    add ajax request to singlerecipe with env vars

--- src/pages/SingleRecipe.js
+++ src/pages/SingleRecipe.js
@@ -7,10 +7,27 @@ export default class SingleRecipe extends Component {
     super(props);
     const id = this.props.match.params.id;
     this.state = {
-      recipe: recipeData,
+      // recipe: recipeData,
+      recipe: {},
       id,
-      loading: false
+      loading: true
     };
+  }
+
+  async componentDidMount() {
+    const url = `https://www.food2fork.com/api/get?key=${
+      process.env.REACT_APP_API_KEY
+    }&rId=${this.state.id}`;
+    try {
+      const response = await fetch(url);
+      const responseData = await response.json();
+      this.setState({
+        recipe: responseData.recipe,
+        loading: false
+      });
+    } catch (e) {
+      console.log(e);
+    }
   }
   render() {
     const {
