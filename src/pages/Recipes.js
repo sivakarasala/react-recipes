8de309128f9274c6661c892c2fe5b810ab85f3e2
shambho <Sivakrishna.Karasala@eku.edu>
2019-03-23 21:48:51 -0400

    add search feature

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -13,15 +13,28 @@ export default class Recipes extends Component {
     search: "",
     url: `https://www.food2fork.com/api/search?key=${
       process.env.REACT_APP_API_KEY
-    }`
+    }`,
+    base_url: `https://www.food2fork.com/api/search?key=${
+      process.env.REACT_APP_API_KEY
+    }`,
+    query: "&q=",
+    error: ""
   };
   async getRecipes() {
     try {
       const data = await fetch(this.state.url);
       const jsonData = await data.json();
-      this.setState({
-        recipes: jsonData.recipes
-      });
+      if (jsonData.recipes.length === 0) {
+        this.setState({
+          error:
+            "sorry but your search did not return any recipes, please try again or press search icon for the most popular recipes"
+        });
+      } else {
+        this.setState({
+          recipes: jsonData.recipes,
+          error: ""
+        });
+      }
     } catch (e) {
       console.log(e);
     }
@@ -36,6 +49,14 @@ export default class Recipes extends Component {
   };
   handleSubmit = e => {
     e.preventDefault();
+    const { base_url, query, search } = this.state;
+    this.setState(
+      {
+        url: `${base_url}${query}${search}`,
+        search: ""
+      },
+      () => this.getRecipes()
+    );
   };
   render() {
     return (
